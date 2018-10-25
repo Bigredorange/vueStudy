d2e14ca9452ef8e7a45e272f2cc03167c8879ee0
cdh <chengdonghai1@gmail.com>
2018-10-25 19:08:56 +0800

    路由跳转加入progress bar

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -2,6 +2,7 @@ import api from './apis'
 import Vue from 'vue'
 import loading from '@/components/loading/index'
 import {Loading} from 'element-ui'
+import '../router/permission';
 
 Vue.prototype.$api = api
 Vue.prototype.$loading = Loading;
--- src/router/permission.js
+++ src/router/permission.js
@@ -1,5 +1,15 @@
-import route from './index'
-
-// route.beforeEach((from, to, next) => {
-
-// })
+import route from './index';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+NProgress.configure({showSpinner: true});
+route.beforeEach((from, to, next) => {
+    NProgress.start();
+    if(to.path== '/home') {
+        next();
+        NProgress.done();
+    }
+    else {
+        next();
+        NProgress.done();
+    }
+})
