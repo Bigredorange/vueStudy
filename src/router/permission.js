58bbdb9efc5b31f8768fa4479bcd33da95e8f256
cdh <chengdonghai1@gmail.com>
2018-11-01 11:35:16 +0800

    修改页面跳转、404页面样式

--- src/router/permission.js
+++ src/router/permission.js
@@ -4,15 +4,20 @@ import 'nprogress/nprogress.css';
 NProgress.configure({showSpinner: true});
 route.beforeEach((from, to, next) => {
     NProgress.start();
-    if(to.path== '/main/home') {
-        console.log(to.path);
-        next({...to,replace:true});
-        NProgress.done();
-    }
-    else {
-        console.log(to.path);
-        next();
-        // next({path:to.path,replace:true});
-        NProgress.done();
-    }
+    next();
+    // if(to.path== '/main/home') {
+    //     console.log(to.path);
+    //     next({...to,replace:true});
+    //     NProgress.done();
+    // }
+    // else {
+    //     console.log(to.path);
+    //     next();
+    //     // next({path:to.path,replace:true});
+    //     NProgress.done();
+    // }
+})
+
+route.afterEach((to,from) => {
+    NProgress.done();
 })
