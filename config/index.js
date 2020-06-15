393ed972a8e38e54a21b50a6bd8e965fa8d1ba91
chengdonghai <chengdonghai@icinfo.com.cn>
2020-06-15 19:33:12 +0800

    更改打包后路径

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vueStudy',
     proxyTable: {},
 
     // Various Dev Server settings
